3826596031de17af4081e884844eaa2b3f190e66
pratamaz <ahanifpratama@gmail.com>
2022-09-22 12:14:28 +0700

    update

--- Sesi-02/slice/slice.go
+++ Sesi-02/slice/slice.go
@@ -41,4 +41,73 @@ func main() {
 	fmt.Println("====================================")
 	fmt.Println()
 
+
+
+	// Fungsi Copy()
+	var fruits1 = []string{"Apple", "Banana", "Mango"}
+	var fruits2 = []string{"Durian", "Pineapple", "Starfruit"}
+
+	nn := copy(fruits1, fruits2)
+
+	fmt.Println("Fruits1 =>", fruits1)
+	fmt.Println("Fruits2 =>", fruits2)
+	fmt.Println("Copied Elements =>", nn)
+
+	fmt.Println("====================================")
+	fmt.Println()
+
+
+	// Slicing Pada Slice
+	var buah1 = []string{"Apel", "Pisang", "Durian", "Nanas"}
+	
+	var buah2 = buah1[1:4]
+	fmt.Printf("%#v\n", buah2)
+
+	var buah3 = buah1[0:]
+	fmt.Printf("%#v\n", buah3)
+
+	var buah4 = buah1[:3]
+	fmt.Printf("%#v\n", buah4)
+
+	var buah5 = buah1[:]
+	fmt.Printf("%#v\n", buah5)
+
+	fmt.Println("====================================")
+	fmt.Println()
+
+
+	// Kombinasi slicing dan append
+	fruits1 = append(fruits1[1:3], "Avocado")
+
+	fmt.Printf("%#v\n", fruits1)
+
+	fmt.Println("====================================")
+	fmt.Println()
+
+
+	// Backing Array
+	var fruits3 = fruits1[2:4]
+
+	fruits3[0] = "Melon"
+
+	fmt.Println("Fruits1 => ", fruits1)
+	fmt.Println("Fruits3 => ", fruits3)
+
+	fmt.Println("====================================")
+	fmt.Println()
+
+
+	// Membuat Backing Array
+	cars := []string{"Ford", "Honda", "Audi", "Range Rover"}
+	newCars := []string{}
+
+	newCars = append(newCars, cars[0:2]...)
+
+	cars[0] = "Nissan"
+
+	fmt.Println("Cars:", cars)
+	fmt.Println("newCars:", newCars)
+
+	fmt.Println("====================================")
+	fmt.Println()
 }
